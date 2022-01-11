60d9964bbd6c6b4918f8c5cb388d9822bad9d73d
liqiubo <liqiubou@nbdeli.com>
2022-01-11 19:46:02 +0800

    initBinder测试

---
+++ src/main/java/com/example/springboot/controller/ModelAttributeController.java
@@ -0,0 +1,46 @@
+package com.example.springboot.controller;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * @author lqb
+ * @date 2022/1/11 19:16
+ */
+@RestController
+public class ModelAttributeController {
+
+    @ModelAttribute
+    public void addString(ModelAndView model) {
+        model.getModel().put("user","user");
+    }
+
+    @GetMapping("/getUser")
+    @CrossOrigin
+    public String getUser(@ModelAttribute String user, ModelAndView modelAndView){
+        Object user1 = modelAndView.getModel().get("user");
+        return (String) user1;
+    }
+
+    @RequestMapping("/initBinder")
+    public String initBinder(Date date){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String format = simpleDateFormat.format(date);
+        return format;
+    }
+
+    @InitBinder
+    public void init(WebDataBinder webDataBinder){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        CustomDateEditor customDateEditor = new CustomDateEditor(simpleDateFormat, true);
+        webDataBinder.registerCustomEditor(Date.class,customDateEditor);
+    }
+}
