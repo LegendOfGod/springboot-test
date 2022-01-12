c80f0c1dce664e058f833670d5962f06eed1534d
liqiubo <liqiubou@nbdeli.com>
2022-01-12 14:21:42 +0800

    springboot async测试

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @PropertySource(value = {"classpath:custom.properties"})
+@EnableAsync
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootApplication.class, args);
-        applicationContext.close();
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 
 }
--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.springboot;
 
+import com.example.springboot.async.Task;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.Future;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
+    @Autowired
+    private Task task;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        Future<String> future1 = task.doTaskOne();
+        Future<String> future2 = task.doTaskTwo();
+        Future<String> future3 = task.doTaskThree();
+        System.out.println(future1.get());
+        System.out.println(future2.get());
+        System.out.println(future3.get());
     }
 
 }
